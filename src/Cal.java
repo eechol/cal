dcdd0c9ec5435602dfb5560981ca5c0bedf22de2
eechol <726154403@qq.com>
2021-01-24 00:22:47 +0800

    modify

--- src/Cal.java
+++ src/Cal.java
@@ -1,9 +1,9 @@
 public class Cal {
     public static void main(String[] args) {
-
+        
     }
 
-    public void reverse(Node h){
+    public static void reverse(Node h){
         if (null == h || null == h.next) return;
         Node pre = null;
         Node tmp = null;
@@ -15,6 +15,21 @@ public class Cal {
             cur = tmp;
         }
     }
+
+    public static void swim(int[] arr ,int k){
+
+    }
+
+    public static void sink(int[] arr, int k){
+
+    }
+
+    public static void swap(int[] arr, int m, int n){
+        int tmp = arr[m];
+        arr[m] = arr[n];
+        arr[n] = tmp;
+    }
+
 }
 
 class Node{

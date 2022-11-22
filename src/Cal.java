a9ef67d389b5331e9314a5aad17d68cd28ffff9a
eechol <627154403@qq.com>
2022-11-22 23:44:39 +0800

    update

--- src/Cal.java
+++ src/Cal.java
@@ -167,32 +167,28 @@ public class Cal {
         }
     }
 
-    public static void swim(int[] arr, int k, int len) {
-        while (k >= 0) {
-            k = len / 2 - 1;
-            if (2 * k + 3 > len) {
-                if (bigThan(arr[2 * k + 1], arr[k])) {
-                    return;
-                } else {
-                    swap(arr, 2 * k + 1, k);
-                }
-            } else {
-                if (bigThan(arr[2 * k + 1], arr[2 * k + 2])) {
-                    if (bigThan(arr[2 * k + 1], arr[k])) {
-                        swap(arr, 2 * k + 1, k);
-                    }
-                } else {
-                    if (bigThan(arr[2 * k + 2], arr[k])) {
-                        swap(arr, 2 * k + 2, k);
-                    }
-                }
-            }
-        }
-    }
-
-    public static void sink(int[] arr, int k, int len) {
-
-    }
+//    public static void swim(int[] arr, int k, int len) {
+//        while (k >= 0) {
+//            k = len / 2 - 1;
+//            if (2 * k + 3 > len) {
+//                if (bigThan(arr[2 * k + 1], arr[k])) {
+//                    return;
+//                } else {
+//                    swap(arr, 2 * k + 1, k);
+//                }
+//            } else {
+//                if (bigThan(arr[2 * k + 1], arr[2 * k + 2])) {
+//                    if (bigThan(arr[2 * k + 1], arr[k])) {
+//                        swap(arr, 2 * k + 1, k);
+//                    }
+//                } else {
+//                    if (bigThan(arr[2 * k + 2], arr[k])) {
+//                        swap(arr, 2 * k + 2, k);
+//                    }
+//                }
+//            }
+//        }
+//    }
 
     public static void swap(int[] arr, int m, int n) {
         int tmp = arr[m];
